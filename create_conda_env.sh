a1747fed1c170c094750f5c2dab370315655f2b2
gsilvi <giorgio.silvi@quantumsimulations.de>
2021-09-08 09:49:07 +0200

    update on conda env creation

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-#conda env create -f environment.yml #doesn`work
-conda create -n NEASQC4 python=3.9
+conda env create -f environment.yml #doesn`work
 conda activate NEASQC4
-pip install qiskit-aer==0.8.2 matplotlib ipykernel
 pip install  git+ssh://git@github.com/NEASQC/hqs_qs_nature.git
 pip install  git+ssh://git@github.com/NEASQC/az_qs_terra.git
