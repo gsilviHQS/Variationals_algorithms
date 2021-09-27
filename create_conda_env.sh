5a6eacee606505567276f47e3e117cd5cb9b1f28
gsilvi <giorgio.silvi@quantumsimulations.de>
2021-09-27 15:58:31 +0200

    update create conda env to install qiskit-terra from main repo ( at spefific commit)

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 conda env create -f environment.yml 
 conda activate NEASQC4
-#pip install  git+ssh://git@github.com/NEASQC/hqs_qs_nature.git
+##pip install  git+ssh://git@github.com/NEASQC/hqs_qs_nature.git
+##pip install  git+ssh://git@github.com/NEASQC/az_qs_terra.git
 pip install git+https://github.com/Qiskit/qiskit-nature.git@c61fcf0afe098d5d133cfff584971b6d39f9a75c
-pip install  git+ssh://git@github.com/NEASQC/az_qs_terra.git
+pip install git+https://github.com/Qiskit/qiskit-terra.git@0da9b6bced12af9589a35b574a550a2c7b12c614
+
