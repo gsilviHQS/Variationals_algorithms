034653738ba59b02eadcd4ef881756c1034b249b
Kovyrshin, Arseny <arseny.kovyrshin@astrazeneca.com>
2021-10-14 12:38:43 +0200

    typos cleaning

--- misc/notebooks/qiskit_mod/neoh2evo.py
+++ misc/notebooks/qiskit_mod/neoh2evo.py
@@ -104,7 +104,7 @@ def aux_print(res):
     
 vqe.initial_point = [-7.8,4.7,1.5,6.2,0.,4.3,6.2,0.,-4.2,-3.1,3.2,4.3,-5.6,2.1,0.9,4.3,3.1,-3.1,6.2,-0.1,-3.1,1.5,-3.1,-3.1,6.0,-1.3,1.2,5.8,2.2,4.7,5.7,-1.5,0.,6.2,6.2,3.3,3.1,1.5,-6.2,3.1,3.8,-3.4,-2.9,1.8,-3.0,-3.7,2.8,1.8,4.7,-1.5,1.5,-3.1,-3.1,0.6,-3.1,-6.2,1.5,0.,-5.1,1.1,-2.6,2.9,5.3,-2.2]
 vqe_en_op = mix_en_op.reduce()
-result = vqe.compute_evolve(vqe_en_op, isteps=20, rsteps=40 di=0.1, dr=0.01)
+result = vqe.compute_evolve(vqe_en_op, isteps=20, rsteps=40, di=0.1, dr=0.01)
 aux_print(result)
 #
 #vqe.initial_point=result.optimal_point
