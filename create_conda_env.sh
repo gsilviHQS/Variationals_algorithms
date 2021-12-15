7f8d0e2e12467eb2f6b7279447e307e185643375
gsilvi <giorgio.silvi@quantumsimulations.de>
2021-12-15 11:20:33 +0100

    modified the conda env to include the modified version of myqlm-interop for qiskit. 2 bugs were present in the interop that should be fixed now

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -4,5 +4,6 @@ conda activate NEASQC4featMYQLM
 pip install git+https://github.com/Qiskit/qiskit-nature.git@c61fcf0afe098d5d133cfff584971b6d39f9a75c
 ##pip install git+https://github.com/Qiskit/qiskit-terra.git@0da9b6bced12af9589a35b574a550a2c7b12c614
 pip install myqlm
-pip install myqlm-interop[qiskit_binder]
+#pip install myqlm-interop[qiskit_binder]
+pip install -e myqlm-interop/
 pip install -e ./
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author="Arseny Kovyrshin, Giorgio Silvi",
     license="European Union Public License 1.2",
 
-    packages=find_packages(exclude=("my_lib",)),
+    packages=find_packages(exclude=("my_lib","myqlm-interop")),
     #install_requires=["numpy","pytest","qiskit_nature=0.20.0","qiskit_terra=0.18.2","qiskit_aer=0.9.0"],
     install_requires=["numpy","pytest","qiskit_nature","qiskit_terra","qiskit_aer"],
     # Don't change these two lines
