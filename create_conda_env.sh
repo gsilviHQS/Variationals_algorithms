4b25248ec39624b3883116aadd8a30bba8d14265
gsilvi <giorgio.silvi@quantumsimulations.de>
2022-06-27 17:06:16 +0200

    minor improvements

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 conda env create --force -f environment.yml 
 conda activate NEASQC4wMYQLM
-pip install git+https://github.com/Qiskit/qiskit-nature.git@c61fcf0afe098d5d133cfff584971b6d39f9a75c
+conda install pip
+$CONDA_PREFIX/bin/pip install git+https://github.com/Qiskit/qiskit-nature.git@c61fcf0afe098d5d133cfff584971b6d39f9a75c
 ##pip install git+https://github.com/Qiskit/qiskit-terra.git@0da9b6bced12af9589a35b574a550a2c7b12c614
-pip install myqlm-interop[qiskit_binder]
-pip install -e ./
+$CONDA_PREFIX/bin/pip install myqlm-interop[qiskit_binder]
+$CONDA_PREFIX/bin/pip install -e ./
